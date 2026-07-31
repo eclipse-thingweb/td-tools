45fb759c67766c7b39d96679ef572ce91b18aa5e
Ege Korkan <egekorkan@gmail.com>
2026-07-31 14:06:05 +0200

    tests: use new counter

--- node/aas-aid/test/asset-interfaces-description-test.ts
+++ node/aas-aid/test/asset-interfaces-description-test.ts
@@ -95,7 +95,7 @@ export class AssetInterfaceDescriptionTest {
             .to.have.lengthOf(1);
         expect(tdObj.properties.count.forms[0])
             .to.have.property("href")
-            .to.eql("http://plugfest.thingweb.io:8083/counter" + "/properties/count");
+            .to.eql("https://plugfest.thingweb.io/counter" + "/properties/count");
         expect(tdObj.properties.count.forms[0]).to.have.property("htv:methodName").to.eql("GET");
         expect(tdObj.properties.count.forms[0]).to.have.property("contentType").to.eql("application/json");
         expect(tdObj.properties.count.forms[0]).not.to.have.property("security");
@@ -115,7 +115,7 @@ export class AssetInterfaceDescriptionTest {
             .to.have.lengthOf(1);
         expect(tdObj.properties.countAsImage.forms[0])
             .to.have.property("href")
-            .to.eql("http://plugfest.thingweb.io:8083/counter" + "/properties/countAsImage");
+            .to.eql("https://plugfest.thingweb.io/counter" + "/properties/countAsImage");
         expect(tdObj.properties.countAsImage.forms[0]).to.have.property("htv:methodName").to.eql("GET");
         expect(tdObj.properties.countAsImage.forms[0]).to.have.property("contentType").to.eql("image/svg+xml");
         expect(tdObj.properties.countAsImage.forms[0]).not.to.have.property("security");
@@ -135,7 +135,7 @@ export class AssetInterfaceDescriptionTest {
             .to.have.lengthOf(1);
         expect(tdObj.properties.redDotImage.forms[0])
             .to.have.property("href")
-            .to.eql("http://plugfest.thingweb.io:8083/counter" + "/properties/redDotImage");
+            .to.eql("https://plugfest.thingweb.io/counter" + "/properties/redDotImage");
         expect(tdObj.properties.redDotImage.forms[0]).to.have.property("htv:methodName").to.eql("GET");
         expect(tdObj.properties.redDotImage.forms[0]).to.have.property("contentType").to.eql("image/png;base64");
         expect(tdObj.properties.redDotImage.forms[0]).not.to.have.property("security");
@@ -165,7 +165,7 @@ export class AssetInterfaceDescriptionTest {
             .to.have.lengthOf(1);
         expect(tdObj.properties.lastChange.forms[0])
             .to.have.property("href")
-            .to.eql("http://plugfest.thingweb.io:8083/counter" + "/properties/lastChange");
+            .to.eql("https://plugfest.thingweb.io/counter" + "/properties/lastChange");
         expect(tdObj.properties.lastChange.forms[0]).to.have.property("htv:methodName").to.eql("GET");
         expect(tdObj.properties.lastChange.forms[0]).to.have.property("contentType").to.eql("application/json");
         expect(tdObj.properties.lastChange.forms[0]).not.to.have.property("security");
@@ -240,7 +240,7 @@ export class AssetInterfaceDescriptionTest {
             .to.have.lengthOf(1);
         expect(tdObj.properties.count.forms[0])
             .to.have.property("href")
-            .to.eql("http://plugfest.thingweb.io:8083/counter" + "/properties/count");
+            .to.eql("https://plugfest.thingweb.io/counter" + "/properties/count");
         expect(tdObj.properties.count.forms[0]).to.have.property("htv:methodName").to.eql("GET");
         expect(tdObj.properties.count.forms[0]).to.have.property("contentType").to.eql("application/json");
         expect(tdObj.properties.count.forms[0]).not.to.have.property("security");
@@ -1309,7 +1309,7 @@ export class AssetInterfaceDescriptionTest {
 
     @test.skip async "should correctly transform counter TD into JSON AAS"() {
         // built-in fetch requires Node.js 18+
-        const response = await fetch("http://plugfest.thingweb.io:8083/counter");
+        const response = await fetch("https://plugfest.thingweb.io/counter");
         const counterTD = await response.json();
 
         const sm = this.assetInterfacesDescription.transformTD2AID(JSON.stringify(counterTD), { createAAS: true }, [
